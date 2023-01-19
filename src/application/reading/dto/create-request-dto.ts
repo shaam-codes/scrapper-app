18697643532b3d9276f308056638ba62f84b4719
Sharmilan <lan.sharmi@gmail.com>
2023-01-19 17:36:27 +0530

    feat: implement dto for reading module

---
+++ src/application/reading/dto/create-request-dto.ts
@@ -0,0 +1,67 @@
+/**
+ * This is application level Data Transfer Object class
+ * Can create section model using this DTO
+ * This class specifically using on section creation
+ */
+
+import Uuid from '../../../domain/uuid';
+
+class CreateRequestDto {
+  private readonly id: Uuid;
+  private readonly value: number;
+  private readonly batchId: Uuid;
+  private readonly detectorId: Uuid;
+  private readonly requestedAt: Date;
+  private readonly receivedAt: Date;
+  private readonly isCorrupted: boolean;
+
+  public constructor(
+        id: Uuid,
+        value: number,
+        batchId: Uuid,
+        detectorId: Uuid,
+        requestedAt: Date,
+        receivedAt: Date,
+        isCorrupted: boolean,
+    ) {
+    this.id = id;
+    this.value = value;
+    this.batchId = batchId;
+    this.detectorId = detectorId;
+    this.requestedAt = requestedAt;
+    this.receivedAt = receivedAt;
+    this.isCorrupted = isCorrupted;
+  }
+
+  public getId(): Uuid {
+    return this.id;
+  }
+    
+    public getValue(): number {
+        return this.value;
+    }
+
+    public getBatchId(): Uuid {
+        return this.batchId;
+    }
+
+    public getDetectorId(): Uuid {
+        return this.detectorId;
+    }
+
+    public getRequestedAt(): Date {
+        return this.requestedAt;
+    }
+
+    public getReceivedAt(): Date {
+        return this.receivedAt;
+    }
+
+    public getIsCorrupted(): boolean {
+        return this.isCorrupted;
+    }
+    
+}
+
+export default CreateRequestDto;
+
---
+++ src/application/reading/dto/create-response-dto.ts
@@ -0,0 +1,36 @@
+/**
+ * This is application level Data Transfer Object class
+ * Can create section model using this DTO
+ * This class specifically using on section creation
+ */
+
+import Uuid from '../../../domain/uuid';
+
+class CreateResponseDto {
+  id: Uuid;
+  name: string;
+  batchId: Uuid;
+  detectorId: Uuid;
+  isActive: boolean;
+  createdAt: Date;
+
+  public constructor(
+        id: Uuid,
+        name: string,
+        batchId: Uuid,
+        detectorId: Uuid,
+        isActive: boolean,
+        createdAt: Date,
+    ) {
+    this.id = id;
+    this.name = name;
+    this.batchId = batchId;
+    this.detectorId = detectorId;
+    this.isActive = isActive;
+    this.createdAt = createdAt;
+  }
+
+}
+
+export default CreateResponseDto;
+
