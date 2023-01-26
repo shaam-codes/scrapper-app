13b90ef50ceb5760e92e84df2ad50e0b37a8e572
Sharmilan <lan.sharmi@gmail.com>
2023-01-26 13:46:42 +0530

    feat: implement prima functions and some db dummy files

--- src/infrastructure/primary-adapters/user/modification-use-case.api.ts
+++ src/infrastructure/primary-adapters/user/modification-use-case.api.ts
@@ -5,13 +5,24 @@
  * This will use inside the api/controller
  */
 
-import CreateRequestDto from '@application/user/dto/create-request-dto';
-import ModificationUseCase from '@application/user/modificaiton-use-case';
+import CreateRequestDto from '../../../application/user/dto/create-request-dto';
+import ModificationUseCase from '../../../application/user/modificaiton-use-case';
+import Uuid from '../../../domain/uuid';
 
 class ModificationUseCaseApi implements ModificationUseCase {
 
   createUser(firstName: string, lastName: string, email: string, password: string): CreateRequestDto {
-    throw new Error('Method not implemented.');
+    return new CreateRequestDto(
+      Uuid.generate(),
+      firstName,
+      lastName,
+      email,
+      password,
+      3,
+      true,
+      'dsfsd',
+      new Date,
+    );
   }
 
 }
--- src/infrastructure/secondary-adapters/postgres/postgre-sql-user-respository.ts
+++ src/infrastructure/secondary-adapters/postgres/postgre-sql-user-respository.ts
@@ -1,9 +1,34 @@
+import { Prisma, User as PrismaUser } from '@prisma/client';
+
 import User from '@domain/user';
 import UserRepository from '@domain/user-repository';
+import { prisma } from '../../../db/prisma-client';
+
+const createUserPrisma = async (input: Prisma.UserCreateInput) => {
+  return (await prisma.user.create({
+    data: input,
+  })) as PrismaUser;
+};
 
 class PostgreSqlUserRepository implements UserRepository {
-  createUser(user: User): void {
-    throw new Error('Method not implemented.');
+  async createUser(user: User): Promise<void> {
+    const __user = await createUserPrisma(
+      {
+        id: 'sdsad3fgda',
+        firstName: 'sdfbgldsjhkfbjk',
+        lastName: 'asdsadasd',
+        email: 'khsdbgkhjdsbflakjs',
+        password: 'hsjdbfjkhsdbfljksd',
+        roleId: 3,
+        isActive: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    );
+
+    console.log(typeof __user);
+
+    return;
   }
 
 }
